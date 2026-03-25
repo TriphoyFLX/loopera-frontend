04bbfec5706fe65b222dc606214dad12a49c1646
Матвеев Дмитрий Дмитриевич <matveevdima@MacBook-Air-Matveev.local>
2026-03-25 20:58:20 +0600

    fdsf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()], // никаких jsxDev, плагин сам понимает dev/prod
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,

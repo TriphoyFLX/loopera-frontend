4535668e7df62d499bf35d5d04ff57fb4cd1c50e
Матвеев Дмитрий Дмитриевич <matveevdima@MacBook-Air-Matveev.local>
2026-03-30 20:47:51 +0600

    asds

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // никаких jsxDev, плагин сам понимает dev/prod
+  plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-        },
-      },
-    },
+    target: 'esnext',
   },
   server: {
     host: true,
